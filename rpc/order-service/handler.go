b14bc066e417eb369796fb7c39121c1c4ac1b063
fltb <fltbk@outlook.com>
2024-12-04 20:43:58 +0800

    feat: order ret baseerr res

--- rpc/order-service/handler.go
+++ rpc/order-service/handler.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	"github.com/hewo/tik-shop/db/superquery"
+	"github.com/hewo/tik-shop/kitex_gen/hewo/tikshop/base"
 	order "github.com/hewo/tik-shop/kitex_gen/hewo/tikshop/order"
 )
 
@@ -13,7 +16,7 @@ type OrderServiceImpl struct{}
 func (s *OrderServiceImpl) SubmitOrder(ctx context.Context, request *order.SubmitOrderRequest) (resp *order.SubmitOrderResponse, err error) {
 	resp, err = superquery.SubmitOrder(request)
 	if err != nil {
-		return nil, err
+		return nil, &base.ErrorResponse{Code: consts.StatusInternalServerError, Message: err.Error()}
 	}
 	return
 }
@@ -22,7 +25,7 @@ func (s *OrderServiceImpl) SubmitOrder(ctx context.Context, request *order.Submi
 func (s *OrderServiceImpl) PayOrder(ctx context.Context, request *order.PayOrderRequest) (resp *order.PayOrderResponse, err error) {
 	resp, err = superquery.PayOrder(request)
 	if err != nil {
-		return nil, err
+		return nil, &base.ErrorResponse{Code: consts.StatusInternalServerError, Message: err.Error()}
 	}
 	return
 }
@@ -31,7 +34,7 @@ func (s *OrderServiceImpl) PayOrder(ctx context.Context, request *order.PayOrder
 func (s *OrderServiceImpl) CancelOrder(ctx context.Context, request *order.CancelOrderRequest) (resp *order.CancelOrderResponse, err error) {
 	resp, err = superquery.CancelOrder(request)
 	if err != nil {
-		return nil, err
+		return nil, &base.ErrorResponse{Code: consts.StatusInternalServerError, Message: err.Error()}
 	}
 	return
 }
@@ -40,7 +43,7 @@ func (s *OrderServiceImpl) CancelOrder(ctx context.Context, request *order.Cance
 func (s *OrderServiceImpl) GetOrders(ctx context.Context, request *order.GetOrdersRequest) (resp *order.GetOrdersResponse, err error) {
 	resp, err = superquery.GetOrders(request)
 	if err != nil {
-		return nil, err
+		return nil, &base.ErrorResponse{Code: consts.StatusInternalServerError, Message: err.Error()}
 	}
 	return
 }
@@ -49,7 +52,7 @@ func (s *OrderServiceImpl) GetOrders(ctx context.Context, request *order.GetOrde
 func (s *OrderServiceImpl) GetOrderById(ctx context.Context, request *order.GetOrderByIdRequest) (resp *order.GetOrderByIdResponse, err error) {
 	resp, err = superquery.GetOrderById(request)
 	if err != nil {
-		return nil, err
+		return nil, &base.ErrorResponse{Code: consts.StatusInternalServerError, Message: err.Error()}
 	}
 	return
 }
